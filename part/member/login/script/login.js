9dde907dba2dbd7c76b4c1dd47bad96b0197d3fa
dbsrlwns <rjswmr@naver.com>
2021-10-20 23:20:17 +0900

    수정

--- part/member/login/script/login.js
+++ part/member/login/script/login.js
@@ -46,6 +46,29 @@ $(function (){
         }
     });
 
+    $(".nonBtn").click(function (){
+        let orderName = $("#orderName").val();
+        let orderIndex = $("#orderIndex").val();
+        let orderPw = $("#orderPw").val();
+        
+        if(orderName == ""){
+            alert("주문자명을 입력해 주세요.");
+            $("#orderName").focus();
+        } else if(orderIndex == ""){
+            alert("주문번호를 입력해 주세요.");
+            $("#orderIndex").focus();
+        } else if(orderPw == ""){
+            alert("비회원 비밀번호를 입력해 주세요.");
+            $("#orderPw").focus();
+        } else {
+            alert("주문번호가 일치하지 않습니다.");
+            $("#orderName").val("");
+            $("#orderIndex").val("");
+            $("#orderPw").val("");
+            $("#orderName").focus();
+        }
+    });
+
     // 페이스북 윈도우 창
     $(".facebook").click(function (){
         let url = "https://www.facebook.com/login.php?skip_api_login=1&api_key=705534492950181&kid_directed_site=0&app_id=705534492950181&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fresponse_type%3Dcode%26client_id%3D705534492950181%26scope%3Dpublic_profile%252C%2Bemail%26state%3D0db45046525b0dcb733d9752631ecfc6f29ca73ec6032659c9fb7e40ac91280102a958044486235cb4703ecc60abacd6dc0f7a5c774e2ff9dc62a56cf47c4eea0cd78f011cc239760ec1e017073be82db64a39e10196624dfaebdc3dd04bc61d2c8c32c267c4a6c0c0669b631b71937910ee3ea741df7aa2f9c6e207956bbe0e%26redirect_uri%3Dhttps%253A%252F%252Fwww.look-n-fit.com%252FApi%252FMember%252FOauth2ClientCallback%252Ffacebook%252F%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D01bc16c5-7043-414e-85aa-77f1e1872811%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.look-n-fit.com%2FApi%2FMember%2FOauth2ClientCallback%2Ffacebook%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D0db45046525b0dcb733d9752631ecfc6f29ca73ec6032659c9fb7e40ac91280102a958044486235cb4703ecc60abacd6dc0f7a5c774e2ff9dc62a56cf47c4eea0cd78f011cc239760ec1e017073be82db64a39e10196624dfaebdc3dd04bc61d2c8c32c267c4a6c0c0669b631b71937910ee3ea741df7aa2f9c6e207956bbe0e%23_%3D_&display=page&locale=ko_KR&pl_dbl=0";
--- part/shopping/cart/script/cart.js
+++ part/shopping/cart/script/cart.js
@@ -1,4 +1,30 @@
 $(function (){
+    // 검색 영역
+    $(".icon").click(function (){
+        $(".search").toggleClass('active');
+        $(".clear").toggleClass('active');
+    });
+    $(".clear").click(function (){
+        $("#mySearch").val("");
+    });
+
+    // 스크롤탑 버튼
+    $(".scrollTop").click(function (){
+        $("html, body").animate({scrollTop:0}, 500);
+    });
+
+    // window 스크롤값
+    $(window).scroll(function (){
+        let wScroll = $(this).scrollTop();
+        // console.log(wScroll);
+        if(wScroll >= 300){
+            $(".scrollTop").addClass('active');
+        } else {
+            $(".scrollTop").removeClass('active');
+        }
+    });
+
+    // 장바구니 탭 메뉴
     $(".cart_tab ul li").click(function (){
         $(".cart_tab ul li").removeClass('active');
         $(this).addClass('active');
